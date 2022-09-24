dc4dc1d405bcefe9477afe14c22f514ba4522d45
azure.learnnow <azure.learnnow@gmail.com>
2022-09-25 00:58:24 +0530

    Design Pattern - Singleton Patterns

---
+++ Singleton/DesignPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Logger obj1 = Logger.GetLogger();
+            Logger Obj2 = Logger.GetLogger();
+
+            Console.WriteLine("This is the hashcode for obj1 = " + obj1.GetHashCode());
+            Console.WriteLine("This is the hashcode for obj2 = " + Obj2.GetHashCode());
+        }
+    }
+
+    //Example of singleton
+    public class Logger
+    {
+        private static Logger instance;
+
+        public static Logger GetLogger()
+        {
+            if(instance==null)
+            {
+                instance = new Logger();
+            }
+            return instance;
+        }
+    }
+}
