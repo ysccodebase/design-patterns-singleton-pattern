83e27f16ae9c402c4a066bd0b8e243237738a3f2
azure.learnnow <azure.learnnow@gmail.com>
2022-09-28 06:44:12 +0530

    Thread safe singleton design pattern

--- Singleton/DesignPattern/Program.cs
+++ Singleton/DesignPattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace DesignPattern
 {
@@ -6,20 +7,48 @@ namespace DesignPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-           
+            Parallel.Invoke(Method1, Method2, Method3);
             Logger obj1 = Logger.GetLogger();
             Logger Obj2 = Logger.GetLogger();
             Logger Obj3 = Logger.GetLogger();
             //Logger obj4 = new Logger();
             Logger Obj4 = Logger.GetLogger();
 
-
+            
             Console.WriteLine("This is the hashcode for obj1 = " + obj1.GetHashCode());
             Console.WriteLine("This is the hashcode for obj2 = " + Obj2.GetHashCode());
             Console.WriteLine("This is the hashcode for obj3 = " + Obj3.GetHashCode());
             Console.WriteLine("This is the hashcode for obj4 = " + Obj4.GetHashCode());
+            Method1();
+            Method2();
+            Method3();
+
+            /*
+            Logger.DerivedClass obj7 = new Logger.DerivedClass();
+            obj7.LoggerMethod1("This is the method wriiten in Logger class");
+            */
+
+
+
+        }
+
+       static void Method1()
+        {
+            Logger Obj5 = Logger.GetLogger();
+            Console.WriteLine("This is the hashcode for obj5 = " + Obj5.GetHashCode());
+        }
+
+         static void Method2()
+        {
+            Logger Obj6 = Logger.GetLogger();
+            Console.WriteLine("This is the hashcode for obj6 = " + Obj6.GetHashCode());
+        }
+
+        static void Method3()
+        {
+            Logger Obj7 = Logger.GetLogger();
+            Console.WriteLine("This is the hashcode for obj7 = " + Obj7.GetHashCode());
         }
     }
 
@@ -42,12 +71,16 @@ namespace DesignPattern
     */
 
 
-    //Example of singleton class with private constructor
-    public class Logger
+    //Example of singleton class with private constructor and marked as sealed
+    /*
+    public sealed class Logger
     {
+        private static int counter = 0;
+        
         private Logger()
         {
-
+            counter++;
+            Console.WriteLine("This is counter value: " + counter);
         }
 
         private static Logger instance;
@@ -60,5 +93,62 @@ namespace DesignPattern
             }
             return instance;
         }
+
+        public void LoggerMethod1(string msg)
+        {
+            Console.WriteLine("msg");
+        }
+
+        //
+        //public class DerivedClass : Logger
+        //{
+
+        //}
+
+        //
     }
+      */
+
+    //Singleton design pattern
+    public sealed class Logger
+    {
+        private static int counter = 0;
+
+        private Logger()
+        {
+            counter++;
+            Console.WriteLine("This is counter value: " + counter);
+        }
+
+        private static Logger instance;
+
+        private static readonly object lockobject = new object();
+
+        
+        public static Logger GetLogger()
+        {
+            if(instance == null)
+            {
+                lock (lockobject)
+                {
+
+                    if (instance == null)
+                    {
+                        instance = new Logger();
+                    }
+
+                }
+
+            }
+
+            return instance;
+        }
+
+        public void LoggerMethod1(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
+
 }
